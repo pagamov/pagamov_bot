3556aa0e63a429ed9213aa038dfdedde9534623d
pagamov <pagamov@gmail.com>
2025-04-07 17:46:38 +0300

    main bot

--- main.py
+++ main.py
@@ -2,9 +2,19 @@ from telegram import Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, filters, CallbackContext
 from telegram.ext import ContextTypes, Application, CommandHandler, MessageHandler, filters, ConversationHandler
 from queue import Queue
+import httpx
+import asyncio
 
 import os
 from dotenv import load_dotenv
+
+async def test_connection():
+    try:
+        async with httpx.AsyncClient() as client:
+            response = await client.get(f'https://api.telegram.org/bot{os.getenv("TOKEN")}/getMe')
+            print(response.json())
+    except httpx.ConnectError as e:
+        print(f"Connection error: {e}")
 
 
 async def start(update: Update, context: CallbackContext) -> None:
@@ -16,8 +26,9 @@ async def echo(update: Update, context: CallbackContext) -> None:
 async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
     print(f'Update: {update} caused error {context.error}')
 
-def main() -> None:
+async def main() -> None:
     load_dotenv()
+    await test_connection()
     app = Application.builder().token(os.getenv("TOKEN")).build()
 
     app.add_handler(CommandHandler("start", start))
@@ -27,4 +38,4 @@ def main() -> None:
     app.run_polling(poll_interval=0.8)
 
 if __name__ == '__main__':
-    main()
+    asyncio.run(main())
