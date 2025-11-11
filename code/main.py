459d41643bf3bb71de9917ba31a74be153ace13b
pagamov <pagamov@gmail.com>
2025-11-11 09:38:16 +0300

    rebuild

--- code/main.py
+++ code/main.py
@@ -3,288 +3,15 @@ from telegram import Update, ReplyKeyboardRemove
 from telegram import KeyboardButton, ReplyKeyboardMarkup
 from telegram.ext import Application, CommandHandler, ContextTypes, ConversationHandler, MessageHandler
 from telegram.ext import filters
-from typing import Final
-import sqlite3
-import os
-import random
-import time
-import logging
 
-import atexit
-
-# TODO Удалить токен. Переместить его в .env
-
-TOKEN : Final = ""
-LOGGER_VERBOSE : Final = True
-TELEGRAM_MAX_MESSAGE_SIZE : Final = 4096
-
-
-# Можно настроить минимальный уровень вывода в консоль или файл
-# logging.basicConfig(level=logging.INFO, filename="py_log.log",filemode="w")
-# logging.basicConfig(level=logging.INFO)
+from Const import TOKEN, TELEGRAM_MAX_MESSAGE_SIZE
 
 # TODO сделать изменение внутреннего аноним ника
 
-class Database: 
-    """ Родительский класс для работы с базой данных.  Мы не хотим переходить на postgresql!
-    Мы фанаты sqlite3!
-    """
-    def __init__(self):
-        self.path = os.path.dirname(os.path.abspath(__file__)) + '/main.db'
+from Database.Database import Database
 
-    def firstInitDatabase(self):
-        con = sqlite3.connect(self.path)
-        cur = con.cursor()
-        # Нужна для работы class Logger(Database)
-        cur.execute(f"""
-            CREATE TABLE IF NOT EXISTS bot_log (
-                bot_log_id      INTEGER UNIQUE,
-                datetime        TEXT NOT NULL,
-                text            TEXT NOT NULL,
-                PRIMARY KEY(bot_log_id)
-            );
-        """)
-        # Нужна для работы class User(Database)
-        cur.execute(f"""
-            CREATE TABLE IF NOT EXISTS user (
-                id_user	        INTEGER UNIQUE,
-                tg_username	    TEXT NOT NULL UNIQUE,
-                bot_username	TEXT,
-                user_is_valid   INTEGER DEFAULT 1 CHECK (user_is_valid in (0, 1)),
-                PRIMARY KEY(id_user)
-            );
-        """)
-        # Тут указываются роли пользователей, данные заполняются при init
-        cur.execute(f"""
-            CREATE TABLE IF NOT EXISTS role (
-                id_role	            INTEGER,
-                name_role	        TEXT NOT NULL UNIQUE,
-                description_role	TEXT,
-                PRIMARY KEY(id_role)
-            );
-        """)
-        con.commit()
-
-
-        # Более крутые таблицы
-
-        
-        # Тут указываются какие роли для каких пользователей заведены
-        cur.execute(f"""
-            CREATE TABLE IF NOT EXISTS user_role (
-                id_user_role	INTEGER,
-                user	INTEGER NOT NULL,
-                role	INTEGER NOT NULL,
-                PRIMARY KEY(id_user_role),
-                FOREIGN KEY(role) REFERENCES role(id_role),
-                FOREIGN KEY(user) REFERENCES user(id_user)
-            );
-        """)
-        con.commit()
-
-
-        try:
-            # Заполняем начальные значения для ролей в свежей базе
-            cur.executemany(f"""
-                INSERT INTO role
-                    (name_role, description_role)
-                VALUES 
-                    (?,?);
-            """,    [("admin", "Имеет доступ ко всему контенту"),
-                     ("user", "Начальная роль всех пользователей")])
-            con.commit()
-        except Exception as e:
-            # Если видим ошибку, получается что такие значения есть.
-            print(e)
-
-        cur.close()
-        con.close()
-
-    
-
-
-class Logger(Database):
-    """
-    Данный класс работает с таблицей bot_log
-    Туда я хочу писать события и ошибки бота
-
-    TODO sec_taken - хочу сюда писать кол-во времени которое заняла та или иная операция
-    TODO хочется чтобы при краше, выдавался список ошибок которые были ранее, например 10 до. что привело к ошибке.
-    """
-
-    def __init__(self):
-        super().__init__()
-    
-    def log(self, message : str, level : str = 'INFO', verbose : bool = LOGGER_VERBOSE) -> None:
-        """
-        level = INFO | ERROR | WARNING | CRITICAL
-        """
-        assert message != '', "message is empty"
-        assert level in ['INFO', 'ERROR', 'WARNING', 'CRITICAL'], f"level cant be {level}"
-        con = sqlite3.connect(self.path)
-        cur = con.cursor()
-        cur.execute(f"""
-            INSERT INTO bot_log 
-            (datetime, text)
-            VALUES
-            (datetime(), "{message}")
-        """)
-        con.commit()
-        cur.close()
-        con.close()
-
-        if verbose:
-            if level == "INFO":
-                logging.info(f"{message}")
-            elif level == 'ERROR':
-                logging.error(f"{message}")
-            elif level == 'WARNING':
-                logging.warning(f"{message}")
-            elif level == 'CRITICAL':
-                logging.critical(f"{message}")
-                exit()
-
-    def tail_log_bot(self, n : int) -> list[str] | str:
-        con = sqlite3.connect(self.path)
-        cur = con.cursor()
-        cur.execute("""
-        WITH tmp AS
-            (SELECT * from bot_log
-            ORDER BY bot_log_id DESC
-            LIMIT {})
-        SELECT 
-            datetime, text 
-        from tmp
-        ORDER BY
-            bot_log_id ASC
-        """.format(n))
-        res = cur.fetchall()
-        cur.close()
-        con.close()
-
-        if len(res) == 0:
-            return "Данная таблица пустая"
-        else:
-            return ["{} : {}\n\n".format(row[0], row[1]) for row in res]
-
-class User(Database):
-    """
-    def __createBot_username(self) -> str
-
-    def createUser(self, tg_username : str) -> None
-
-    def get_id_user(self, tg_username : str) -> int
-    def get_bot_username(self, tg_username : str) -> str
-    """
-
-    def __init__(self):
-        super().__init__()
-        self.default_user_role = 'user'
-        self.admin_user_role = 'admin'
-        self.admin_list = ['pagamov']
-
-    def __createBot_username(self) -> str:
-        """
-        Делаем случайное имя для анонимного чата
-        """
-        pril = ["Удачливый","Смелый","Весёлый","Храбрый","Гениальный","Остроумный","Талантливый","Умный","Забавный",
-                "Быстрый","Честный","Осторожный","Решительный","Проницательный","Верный","Любимый","Дерзкий","Очаровательный","Щедрый","Находчивый"]
-        animals = ['слон','тигр','медведь','лев','крокодил','голубь','жираф','верблюд','броненосец','кот']
-        dig = ['0','1','2','3','4','5','6','7','8','9']
-
-        return f"{random.choice(pril)}_{random.choice(animals)}_{''.join([random.choice(dig) for i in range(5)])}"
-
-    def createUser(self, tg_username : str) -> None:
-        assert tg_username != '', "tg_username is empty"
-        con = sqlite3.connect(self.path)
-        cur = con.cursor()
-        cur.execute(f"""
-            INSERT INTO 
-                    user 
-                    (tg_username, bot_username)
-            VALUES 
-                    ("{tg_username}", "{self.__createBot_username()}");
-        """)
-        con.commit()
-        # Ставим пользователю доступ по умолчанию
-        cur.execute(f"""
-            INSERT INTO user_role
-                (user, role)
-            VALUES
-            (
-                (SELECT user.id_user FROM user WHERE user.tg_username = '{tg_username}'), 
-                (SELECT role.id_role FROM role WHERE role.name_role = '{self.default_user_role}')
-            )
-        """)
-
-
-        # Админские штучки
-        if tg_username in self.admin_list:
-            cur.execute(f"""
-                INSERT INTO user_role
-                    (user, role)
-                VALUES
-                (
-                    (SELECT user.id_user FROM user WHERE user.tg_username = '{tg_username}'), 
-                    (SELECT role.id_role FROM role WHERE role.name_role = '{self.admin_user_role}')
-                )
-            """)
-        # 
-        con.commit()
-        cur.close()
-        con.close()
-
-    def get_id_user(self, tg_username : str) -> int:
-        """
-        Вернем id_user из таблицы user по нику из tg. -1 если пользователя нет в базе.
-        """
-        assert tg_username != '', "tg_username is empty"
-
-        con = sqlite3.connect(self.path)
-        cur = con.cursor()
-        cur.execute(f"""
-            SELECT id_user FROM user
-            WHERE tg_username = "{tg_username}"
-        """)
-        res = cur.fetchall()
-        cur.close()
-        con.close()
-        return -1 if len(res) == 0 else res[0][0]
-
-    def is_admin(self, tg_username : str) -> bool:
-        assert tg_username != '', "tg_username is empty"
-
-        con = sqlite3.connect(self.path)
-        cur = con.cursor()
-        cur.execute(f"""
-            SELECT 
-                id_user_role 
-            FROM
-                user_role
-            WHERE 
-                user = (SELECT id_user FROM user WHERE tg_username = '{tg_username}') AND 
-                role = (SELECT id_role FROM role WHERE name_role = '{self.admin_user_role}')
-        """)
-        res = cur.fetchall()
-        cur.close()
-        con.close()
-        return False if len(res) == 0 else True
-
-    def get_bot_username(self, tg_username : str) -> str:
-        assert tg_username != '', "tg_username is empty"
-
-        con = sqlite3.connect(self.path)
-        cur = con.cursor()
-        cur.execute(f"""
-            SELECT bot_username FROM user
-            WHERE tg_username = "{tg_username}"
-        """)
-        res = cur.fetchall()
-        cur.close()
-        con.close()
-        assert res[0][0] != '', "bot_username is empty"
-
-        return res[0][0]
+from Database.Logger.Logger import Logger
+from Database.User.User import User
 
 # Handler section
 
@@ -315,6 +42,7 @@ class Keyboard():
                     [KeyboardButton("Удалить"),             KeyboardButton("Изменить")],
                     [KeyboardButton("Настройки"),           KeyboardButton("Назад")]
                 ], resize_keyboard=True)
+    
     JOB_MENU = ReplyKeyboardMarkup([
                     [KeyboardButton("Мои дела"),            KeyboardButton("Добавить")],
                     [KeyboardButton("Удалить"),             KeyboardButton("Изменить")],
@@ -782,7 +510,6 @@ def main():
     ))
     app.add_error_handler(error)
 
-    
     try:
         logger.log("Polling bot...")
         app.run_polling(poll_interval=0.8, )
