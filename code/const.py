ab971ac86e91ecc74edbc1466088c24b768d43a2
pagamov <pagamov@gmail.com>
2026-02-10 12:49:48 +0300

    const

--- code/const.py
+++ code/const.py
@@ -9,48 +9,47 @@ from telegram import KeyboardButton, ReplyKeyboardMarkup
 load_dotenv()
 
 # Паша если сольешь токен опять - обещаю перестать писать ботов для ВКР
-TOKEN : Final = os.getenv("TOKEN")
+TOKEN: Final = os.getenv("TOKEN")
 
 # Означает, будет ли модуль Logger выводить данные не только в БД но и в консоль
-LOGGER_VERBOSE : Final = True
+LOGGER_VERBOSE: Final = True
 
 # Столько char может быть максимум в одном текстовом сообщении в телеграме
-TELEGRAM_MAX_MESSAGE_SIZE : Final = 4096
+TELEGRAM_MAX_MESSAGE_SIZE: Final = 4096
 
 
 class Text:
     """Тут будут лежать все возможные текстовые константы
     """
-    
+
     start_command_t0 = """chat_id ({})
         tg_username ({}) in start_command {}: {}"""
-        
+
     start_command_t1 = "Добро пожаловать, {}!"
     start_command_t2 = "Добро пожаловать в мое приложение, {}!"
     start_command_t2 += "\nВам доступны напоминания и трекер привычек."
     start_command_t2 += "\nТакже вы можете воспользоваться готовыми программами"
     start_command_t2 += " приобретения привычек или отказа от плохих."
     start_command_t2 += "\nЖелаю удачи!"
-    
+
     start_command_t3 = "Добро пожаловать, о великий равный небу {}."
     start_command_t3 += " Ваши покои вас ждут."
-    
+
     FROM_IDLE_MENU_t0 = "Вернулись после падения сервера (не ваш косяк),"
     FROM_IDLE_MENU_t0 += " о великий равный небу."
-    
+
     FROM_IDLE_MENU_t1 = "Вернулись после падения сервера"
-    
+
     cancel_command_t0 = "Работа бота завершена"
-    
-    MAIN_MENU_KEYBOARD : list[str] = ["Напоминания","_Дела","Привычки",
-                                      "Готовые программы","Обо мне"]
-    
+
+    MAIN_MENU_KEYBOARD: list[str] = ["Напоминания", "_Дела", "Привычки",
+                                     "Готовые программы", "Обо мне"]
+
     MAIN_MENU_t0 = "Система напоминаний"
     MAIN_MENU_t1 = "Система твоих дел"
     MAIN_MENU_t2 = "Система привычек"
     MAIN_MENU_t3 = "Система готовых программ для тебя"
     MAIN_MENU_t4 = "Что то о тебе"
-    
 
     MAIN_MENU_ADMIN_t0 = MAIN_MENU_t0
     MAIN_MENU_ADMIN_t1 = MAIN_MENU_t1
@@ -58,51 +57,51 @@ class Text:
     MAIN_MENU_ADMIN_t3 = MAIN_MENU_t3
     MAIN_MENU_ADMIN_t4 = MAIN_MENU_t4
     MAIN_MENU_ADMIN_t5 = "Ну раз ты админ..."
-    
-    MAIN_MENU_ADMIN_KEYBOARD : list[str] = MAIN_MENU_KEYBOARD \
-                                        + ["Панель Админа"]
-    
-    NOTIFY_MENU_KEYBOARD : list[str] = \
-        ["Мои напоминания","Добавить","Удалить","Изменить","Настройки","Назад"]
-        
-    HABBIT_MENU_KEYBOARD : list[str] = \
-        ["Мои привычки","Добавить","Удалить","Изменить","Настройки","Назад"]
-    
-    PROGRAMM_MENU_KEYBOARD : list[str] = \
-        ["Мои готовые программы","Добавить",
-         "Удалить","Изменить","Настройки","Назад"]
-    
-    ABOUT_ME_MENU_KEYBOARD : list[str] = ["Изменить мой ник", "Назад"]
-    
-    ADMIN_PANEL_1_KEYBOARD : list[str] = \
-        ["Получить логи бота", 
-         "Добавить напоминание через 1 минуту"] + ["Назад",">"]
-    
-    ADMIN_PANEL_2_KEYBOARD : list[str] = [] + ["<","Назад",">"]
-    
-    ADMIN_PANEL_3_KEYBOARD : list[str] = [] + ["<","Назад"]
 
-    ABOUT_ME_CHANGE_NICK_KEYBOARD : list[str] = \
+    MAIN_MENU_ADMIN_KEYBOARD: list[str] = MAIN_MENU_KEYBOARD \
+        + ["Панель Админа"]
+
+    NOTIFY_MENU_KEYBOARD: list[str] = \
+        ["Мои напоминания", "Добавить", "Удалить", "Изменить", "Настройки", "Назад"]
+
+    HABBIT_MENU_KEYBOARD: list[str] = \
+        ["Мои привычки", "Добавить", "Удалить", "Изменить", "Настройки", "Назад"]
+
+    PROGRAMM_MENU_KEYBOARD: list[str] = \
+        ["Мои готовые программы", "Добавить",
+         "Удалить", "Изменить", "Настройки", "Назад"]
+
+    ABOUT_ME_MENU_KEYBOARD: list[str] = ["Изменить мой ник", "Назад"]
+
+    ADMIN_PANEL_1_KEYBOARD: list[str] = \
+        ["Получить логи бота",
+         "Добавить напоминание через 1 минуту"] + ["Назад", ">"]
+
+    ADMIN_PANEL_2_KEYBOARD: list[str] = [] + ["<", "Назад", ">"]
+
+    ADMIN_PANEL_3_KEYBOARD: list[str] = [] + ["<", "Назад"]
+
+    ABOUT_ME_CHANGE_NICK_KEYBOARD: list[str] = \
         ["Попробуем еще раз", "Мне нравится", "Оставить все как было"]
 
-    error_t0 : str = """Update:\n\n{}\n\nCaused error:\n\n{}"""
+    error_t0: str = """Update:\n\n{}\n\nCaused error:\n\n{}"""
 
-    createBot_username_t0 : list[str] = \
-        ["Удачливый","Смелый","Весёлый","Храбрый","Гениальный","Остроумный",
-         "Талантливый","Умный","Забавный","Быстрый","Честный","Осторожный",
-         "Решительный","Проницательный","Верный","Любимый","Дерзкий",
-         "Очаровательный","Щедрый","Находчивый"]
-    
-    createBot_username_t1 : list[str] = \
-        ['слон','тигр','медведь','лев','крокодил',
-         'голубь','жираф','верблюд','броненосец','кот']
-    
-    createBot_username_t2 : list[str] = \
-        ['0','1','2','3','4','5','6','7','8','9']
-        
+    createBot_username_t0: list[str] = \
+        ["Удачливый", "Смелый", "Весёлый", "Храбрый", "Гениальный", "Остроумный",
+         "Талантливый", "Умный", "Забавный", "Быстрый", "Честный", "Осторожный",
+         "Решительный", "Проницательный", "Верный", "Любимый", "Дерзкий",
+         "Очаровательный", "Щедрый", "Находчивый"]
+
+    createBot_username_t1: list[str] = \
+        ['слон', 'тигр', 'медведь', 'лев', 'крокодил',
+         'голубь', 'жираф', 'верблюд', 'броненосец', 'кот']
+
+    createBot_username_t2: list[str] = \
+        ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
     ABOUT_ME_MENU_CHANGE_NICK_t0 = "А что вам не нравится в {}?\n"
     ABOUT_ME_MENU_CHANGE_NICK_t0 += "Сгенерируем вам новый ник...\nКак вам {}?"
-    
+
     ABOUT_ME_MENU_CHANGE_NICK_t1 = 'А как вам {}?'
     ABOUT_ME_MENU_CHANGE_NICK_t2 = "Попробуем еще раз"
     ABOUT_ME_MENU_CHANGE_NICK_t3 = "Оставить все как было"
@@ -115,12 +114,12 @@ class Text:
             bot_username="{}"
         where 
             tg_username="{}";'''
-    
+
     RETURN_TO_MENU_ADMIN = 'Давай в основное меню, великий господин равный небу'
     RETURN_TO_MENU = 'Давай в основное меню'
-    
+
     ADMIN_PANEL_1_t0 = 'Сколько последних записей выдать?'
-    
+
     ADMIN_PANEL_1_t1 = """
         INSERT INTO notify 
             (chat_id, user_id, description, time_create, time_notify)
@@ -129,39 +128,37 @@ class Text:
             (SELECT id_user FROM user WHERE tg_username = "{}" limit 1),
             "Test notify 1 min for {}", DATETIME('NOW'), 
                                         DATETIME('now', '+1 minute'))"""
-    
+
     ADMIN_PANEL_1_t2 = 'Напоминание через 1 мин. добавлено'
     ADMIN_PANEL_1_t3 = ''
     ADMIN_PANEL_1_t4 = ''
-    
+
     ADMIN_PANEL_2_t0 = 'Панелька страничка 1'
     ADMIN_PANEL_2_t1 = 'Панелька страничка 3'
-    
+
     ADMIN_PANEL_3_t0 = 'Панелька страничка 2'
-    
+
     ADMIN_PANEL_forward = '>'
     ADMIN_PANEL_backward = '<'
     ADMIN_PANEL_return = 'Назад'
 
-    createUser_t0 : str = """INSERT INTO user (tg_username, bot_username)
+    createUser_t0: str = """INSERT INTO user (tg_username, bot_username)
                                 VALUES ("{}", "{}");"""
-                                
-                                
+
     NOTIFY_MENU_ADD_DATEPICK_t0 = \
         "Что то не так с вашими данными, попробуйте ввести в виде dd.mm.yy"
-    
-          
+
     NOTIFY_MENU_ADD_TIMEPICK_t0 = \
         "Что то не так с вашими данными, попробуйте ввести в виде hh.mm"
-    
-    createUser_t1 : str = """
+
+    createUser_t1: str = """
         INSERT INTO user_role
             (user, role)
         VALUES(
             (SELECT user.id_user FROM user WHERE user.tg_username = '{}'), 
             (SELECT role.id_role FROM role WHERE role.name_role = '{}'))"""
-    
-    createUser_t2 : str = """
+
+    createUser_t2: str = """
         INSERT INTO user_role
             (user, role)
         VALUES
@@ -169,15 +166,15 @@ class Text:
             (SELECT user.id_user FROM user WHERE user.tg_username = '{}'), 
             (SELECT role.id_role FROM role WHERE role.name_role = '{}'))"""
 
-    get_id_user_t0 : str = '''
+    get_id_user_t0: str = '''
         SELECT 
             id_user 
         FROM 
             user
         WHERE 
             tg_username = "{}";'''
-    
-    is_admin_t0 : str = """
+
+    is_admin_t0: str = """
         SELECT 
             id_user_role 
         FROM
@@ -185,18 +182,18 @@ class Text:
         WHERE 
             user = (SELECT id_user FROM user WHERE tg_username = '{}') AND 
             role = (SELECT id_role FROM role WHERE name_role = '{}');"""
-    
-    get_bot_username_t0 : str = '''
+
+    get_bot_username_t0: str = '''
         SELECT 
             bot_username 
         FROM 
             user
         WHERE 
             tg_username = "{}";'''
-            
-    get_bot_username_err : str = "tg_username is empty"
 
-    tail_log_bot_t0 : str = """
+    get_bot_username_err: str = "tg_username is empty"
+
+    tail_log_bot_t0: str = """
         WITH tmp AS
             (SELECT * from bot_log
             ORDER BY bot_log_id DESC
@@ -208,7 +205,7 @@ class Text:
             bot_log_id ASC;
     """
 
-    firstInitDatabase_message_to_delete : str = """
+    firstInitDatabase_message_to_delete: str = """
         CREATE TABLE IF NOT EXISTS message_to_delete (
             message_to_delete_id    INTEGER UNIQUE,
             datetime_to_delete      TEXT NOT NULL,
@@ -219,7 +216,7 @@ class Text:
         );
     """
 
-    firstInitDatabase_bot_log : str = """
+    firstInitDatabase_bot_log: str = """
         CREATE TABLE IF NOT EXISTS bot_log (
             bot_log_id      INTEGER UNIQUE,
             datetime        TEXT NOT NULL,
@@ -227,7 +224,7 @@ class Text:
             PRIMARY KEY(bot_log_id)
         );"""
 
-    firstInitDatabase_user : str = """
+    firstInitDatabase_user: str = """
         CREATE TABLE IF NOT EXISTS user (
             id_user	        INTEGER UNIQUE,
             tg_username	    TEXT NOT NULL UNIQUE,
@@ -235,16 +232,16 @@ class Text:
             user_is_valid   INTEGER DEFAULT 1 CHECK (user_is_valid in (0, 1)),
             PRIMARY KEY(id_user)
         );"""
-    
-    firstInitDatabase_role : str = """
+
+    firstInitDatabase_role: str = """
         CREATE TABLE IF NOT EXISTS role (
             id_role	            INTEGER,
             name_role	        TEXT NOT NULL UNIQUE,
             description_role	TEXT,
             PRIMARY KEY(id_role)
         );"""
-    
-    firstInitDatabase_notify : str = """
+
+    firstInitDatabase_notify: str = """
         CREATE TABLE IF NOT EXISTS notify (
             id_notify               INTEGER,
             chat_id                 INTEGER NOT NULL,
@@ -257,8 +254,8 @@ class Text:
             PRIMARY KEY(id_notify),
             FOREIGN KEY(user_id) REFERENCES user(id_user)
         );"""
-    
-    firstInitDatabase_user_role : str = """
+
+    firstInitDatabase_user_role: str = """
         CREATE TABLE IF NOT EXISTS user_role (
             id_user_role	INTEGER,
             user	INTEGER NOT NULL,
@@ -268,141 +265,138 @@ class Text:
             FOREIGN KEY(user) REFERENCES user(id_user)
         );"""
 
-    firstInitDatabase_insert_role : str = """
+    firstInitDatabase_insert_role: str = """
         INSERT INTO role
             (name_role, description_role)
         VALUES 
             (?,?);"""
 
-    firstInitDatabase_insert_role_arr : list = [
-                ("admin", "Имеет доступ ко всему контенту"),
-                ("user", "Начальная роль всех пользователей")]
-    
-    log_insert_query : str = """
+    firstInitDatabase_insert_role_arr: list = [
+        ("admin", "Имеет доступ ко всему контенту"),
+        ("user", "Начальная роль всех пользователей")]
+
+    log_insert_query: str = """
             INSERT INTO bot_log
                 (datetime, text)
             VALUES
                 (datetime(), "{}");"""
-                
-                
-                
+
     User_default_user_role = "user"
     User_admin_user_role = "admin"
     User_admin_list = ["pagamov"]
-    
+
     User_t0 = "Создаем пользователя {}"
     User_t1_err = "tg_username is empty"
     User_t2 = "Создаем пользователя {} - права по умолчанию"
     User_t3 = "Создаем пользователя {} - админские штучки"
-    User_t4 = "Ищем пользователя с ником {}"        
+    User_t4 = "Ищем пользователя с ником {}"
     User_t5 = "Проверяем пользователя с ником {} является ли он админом"
     User_t6 = "Ищем внутренний никнейм у пользователя с ником {}"
-                
-                
+
 
 class Keyboard:
     MAIN_MENU = \
         ReplyKeyboardMarkup([
             [KeyboardButton(Text.MAIN_MENU_KEYBOARD[0]),
              KeyboardButton(Text.MAIN_MENU_KEYBOARD[1])],
-            # 
+            #
             [KeyboardButton(Text.MAIN_MENU_KEYBOARD[2]),
              KeyboardButton(Text.MAIN_MENU_KEYBOARD[3])],
-            # 
+            #
             [KeyboardButton(Text.MAIN_MENU_KEYBOARD[4])]
         ], resize_keyboard=True)
-    
+
     MAIN_MENU_ADMIN = \
         ReplyKeyboardMarkup([
             [KeyboardButton(Text.MAIN_MENU_ADMIN_KEYBOARD[0]),
              KeyboardButton(Text.MAIN_MENU_ADMIN_KEYBOARD[1])],
-            # 
+            #
             [KeyboardButton(Text.MAIN_MENU_ADMIN_KEYBOARD[2]),
              KeyboardButton(Text.MAIN_MENU_ADMIN_KEYBOARD[3])],
-            # 
+            #
             [KeyboardButton(Text.MAIN_MENU_ADMIN_KEYBOARD[4]),
              KeyboardButton(Text.MAIN_MENU_ADMIN_KEYBOARD[5])]
         ], resize_keyboard=True)
-    
+
     NOTIFY_MENU = \
         ReplyKeyboardMarkup([
             [KeyboardButton(Text.NOTIFY_MENU_KEYBOARD[0]),
              KeyboardButton(Text.NOTIFY_MENU_KEYBOARD[1])],
-            # 
+            #
             [KeyboardButton(Text.NOTIFY_MENU_KEYBOARD[2]),
              KeyboardButton(Text.NOTIFY_MENU_KEYBOARD[3])],
-            # 
+            #
             [KeyboardButton(Text.NOTIFY_MENU_KEYBOARD[4]),
              KeyboardButton(Text.NOTIFY_MENU_KEYBOARD[5])]
         ], resize_keyboard=True)
-    
+
     HABBIT_MENU = \
         ReplyKeyboardMarkup([
             [KeyboardButton(Text.HABBIT_MENU_KEYBOARD[0]),
              KeyboardButton(Text.HABBIT_MENU_KEYBOARD[1])],
-            # 
+            #
             [KeyboardButton(Text.HABBIT_MENU_KEYBOARD[2]),
              KeyboardButton(Text.HABBIT_MENU_KEYBOARD[3])],
-            # 
+            #
             [KeyboardButton(Text.HABBIT_MENU_KEYBOARD[4]),
              KeyboardButton(Text.HABBIT_MENU_KEYBOARD[5])]
         ], resize_keyboard=True)
-    
+
     PROGRAMM_MENU = \
         ReplyKeyboardMarkup([
             [KeyboardButton(Text.PROGRAMM_MENU_KEYBOARD[0]),
              KeyboardButton(Text.PROGRAMM_MENU_KEYBOARD[1])],
-            # 
+            #
             [KeyboardButton(Text.PROGRAMM_MENU_KEYBOARD[2]),
              KeyboardButton(Text.PROGRAMM_MENU_KEYBOARD[3])],
-            # 
+            #
             [KeyboardButton(Text.PROGRAMM_MENU_KEYBOARD[4]),
              KeyboardButton(Text.PROGRAMM_MENU_KEYBOARD[5])]
         ], resize_keyboard=True)
-    
+
     ABOUT_ME_MENU = \
         ReplyKeyboardMarkup([
             [KeyboardButton(Text.ABOUT_ME_MENU_KEYBOARD[0]),
-            KeyboardButton(Text.ABOUT_ME_MENU_KEYBOARD[1])]
-            # 
-    ], resize_keyboard=True)
-    
-    
+             KeyboardButton(Text.ABOUT_ME_MENU_KEYBOARD[1])]
+            #
+        ], resize_keyboard=True)
+
     ABOUT_ME_CHANGE_NICK = \
         ReplyKeyboardMarkup([
-            [KeyboardButton(Text.ABOUT_ME_CHANGE_NICK_KEYBOARD[0]), 
+            [KeyboardButton(Text.ABOUT_ME_CHANGE_NICK_KEYBOARD[0]),
              KeyboardButton(Text.ABOUT_ME_CHANGE_NICK_KEYBOARD[1])],
-            # 
+            #
             [KeyboardButton(Text.ABOUT_ME_CHANGE_NICK_KEYBOARD[2])]
         ], resize_keyboard=True)
-    
+
     ADMIN_PANEL_1 = \
         ReplyKeyboardMarkup([
-            [KeyboardButton(Text.ADMIN_PANEL_1_KEYBOARD[0]), 
-            KeyboardButton(Text.ADMIN_PANEL_1_KEYBOARD[1])],
-            # 
+            [KeyboardButton(Text.ADMIN_PANEL_1_KEYBOARD[0]),
+             KeyboardButton(Text.ADMIN_PANEL_1_KEYBOARD[1])],
+            #
             [KeyboardButton(Text.ADMIN_PANEL_1_KEYBOARD[-2]),  # Назад
-                KeyboardButton(Text.ADMIN_PANEL_1_KEYBOARD[-1])], # >
+                KeyboardButton(Text.ADMIN_PANEL_1_KEYBOARD[-1])],  # >
         ], resize_keyboard=True)
-    
+
     ADMIN_PANEL_2 = \
         ReplyKeyboardMarkup([
-            # 
+            #
             [KeyboardButton(Text.ADMIN_PANEL_2_KEYBOARD[-3]),   # <
-             KeyboardButton(Text.ADMIN_PANEL_2_KEYBOARD[-2]),   # Назад 
+             KeyboardButton(Text.ADMIN_PANEL_2_KEYBOARD[-2]),   # Назад
              KeyboardButton(Text.ADMIN_PANEL_2_KEYBOARD[-1])],  # >
         ], resize_keyboard=True)
-    
+
     ADMIN_PANEL_3 = \
         ReplyKeyboardMarkup([
             [KeyboardButton(Text.ADMIN_PANEL_3_KEYBOARD[-2]),  # <
-             KeyboardButton(Text.ADMIN_PANEL_3_KEYBOARD[-1])], # Назад
+             KeyboardButton(Text.ADMIN_PANEL_3_KEYBOARD[-1])],  # Назад
         ], resize_keyboard=True)
-    
+
     NOTIFY_MENU_ADD_CANCEL_KEYBOARD = \
         ReplyKeyboardMarkup([
             [KeyboardButton("Отмена")]
         ], resize_keyboard=True)
+
 
 class State(Enum):
     MAIN_MENU = 1
@@ -439,4 +433,3 @@ class State(Enum):
     ADMIN_PANEL_TAIL_LOG_BOT = 700_001
 
     MAIN_MENU_ADMIN = 8
-
