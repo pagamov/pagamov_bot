18bcf62e9dcdfb2837068db799d3e47eb818d0d6
pagamov <pagamov@gmail.com>
2026-02-26 16:46:07 +0300

    code

--- code/database.py
+++ code/database.py
@@ -9,20 +9,22 @@ from const import *
 # logging.basicConfig(level=logging.INFO, filename="py_log.log",filemode="w")
 # logging.basicConfig(level=logging.INFO)
 
-class Database: 
+
+class Database:
     """Родительский класс для работы с базой данных.
-    
+
     Мы не хотим переходить на postgresql!
-    
+
     Мы фанаты sqlite3!
     """
+
     def __init__(self):
         # Мы ищем файл который исполняется
         # Далее отступаем назад и в папке db делаем файл main.db
         self.path = os.path.dirname(os.path.abspath(__file__)) \
-                    + '/../db/main.db'
+            + '/../db/main.db'
 
-    def run_query(self, query : str) -> None:
+    def run_query(self, query: str) -> None:
         con = sqlite3.connect(self.path)
         cur = con.cursor()
         cur.execute(query)
@@ -30,7 +32,7 @@ class Database:
         cur.close()
         con.close()
 
-    def run_many_query(self, query : str, arr : list) -> None:
+    def run_many_query(self, query: str, arr: list) -> None:
         con = sqlite3.connect(self.path)
         cur = con.cursor()
         try:
@@ -42,7 +44,7 @@ class Database:
         cur.close()
         con.close()
 
-    def get_from_query(self, query : str) -> list[any]:
+    def get_from_query(self, query: str) -> list[any]:
         con = sqlite3.connect(self.path)
         cur = con.cursor()
         cur.execute(query)
@@ -54,7 +56,7 @@ class Database:
     def firstInitDatabase(self):
         # Хочется чтобы чат был чистый, тут пишутся сообщения на удаление
         self.run_query(Text.firstInitDatabase_message_to_delete)
-        
+
         # Нужна для работы class Logger(Database)
         self.run_query(Text.firstInitDatabase_bot_log)
 
@@ -73,11 +75,12 @@ class Database:
 
         try:
             # Заполняем начальные значения для ролей в свежей базе
-            self.run_many_query(Text.firstInitDatabase_insert_role, 
+            self.run_many_query(Text.firstInitDatabase_insert_role,
                                 Text.firstInitDatabase_insert_role_arr)
         except Exception as e:
             # Если видим ошибку, получается что такие значения есть.
             print(e)
+
 
 class Logger:
     """Данный класс работает с таблицей bot_log
@@ -88,18 +91,16 @@ class Logger:
     TODO хочется чтобы при краше, выдавался список ошибок \
         которые были ранее, например 10 до. что привело к ошибке.
     """
-    
-    def log(self, message : str, 
-            level : str = 'INFO', 
-            verbose : bool = LOGGER_VERBOSE) -> None:
-        
+
+    def log(self, message: str,
+            level: str = 'INFO',
+            verbose: bool = LOGGER_VERBOSE) -> None:
         """level = INFO | ERROR | WARNING | CRITICAL
         """
-        
+
         assert message != '', "message is empty"
         assert level in ['INFO', 'ERROR', 'WARNING', 'CRITICAL'], \
             f"level cant be {level}"
-
 
         Database().run_query(Text.log_insert_query.format(message))
 
@@ -114,13 +115,14 @@ class Logger:
                 case "CRITICAL":
                     logging.critical(f"{message}")
                     exit()
-                    
-    def tail_log_bot(self, n : int) -> list[str] | str:
+
+    def tail_log_bot(self, n: int) -> list[str] | str:
         res = Database().get_from_query(Text.tail_log_bot_t0.format(n))
         if len(res) == 0:
             return "Данная таблица пустая"
         else:
             return ["{} : {}\n\n".format(row[0], row[1]) for row in res]
+
 
 class User:
     """def createUser(self, tg_username : str) -> None
@@ -128,6 +130,7 @@ class User:
     def get_id_user(self, tg_username : str) -> int
     def get_bot_username(self, tg_username : str) -> str
     """
+
     def __init__(self):
         self.default_user_role = Text.User_default_user_role
         self.admin_user_role = Text.User_admin_user_role
@@ -139,68 +142,68 @@ class User:
         pril = Text.createBot_username_t0
         animals = Text.createBot_username_t1
         dig = Text.createBot_username_t2
-        
+
         result = f"{random.choice(pril)}_" \
-                + f"{random.choice(animals)}_" \
-                + f"{''.join([random.choice(dig) for i in range(5)])}"
+            + f"{random.choice(animals)}_" \
+            + f"{''.join([random.choice(dig) for i in range(5)])}"
 
         return result
 
-    def createUser(self, tg_username : str) -> None:
+    def createUser(self, tg_username: str) -> None:
         """1. Добавляет пользователя в базу
-        
+
         2. Ставит ему базовые права пользователя (роль)
-        
+
         3. Проверяет есть ли ник в списке админов. \
             Если есть - добавляет ему админские права
         """
         assert tg_username != '', Text.User_t1_err
-        
+
         Logger().log(Text.User_t0.format(tg_username))
         Database().run_query(
             Text.createUser_t0.format(tg_username, self.createBot_username()))
-        
+
         # Ставим пользователю доступ по умолчанию
         Logger().log(Text.User_t2.format(tg_username))
         Database().run_query(
             Text.createUser_t1.format(tg_username, self.default_user_role))
-        
+
         # Админские штучки
         if tg_username in self.admin_list:
             Logger().log(Text.User_t3.format(tg_username))
             Database().run_query(
                 Text.createUser_t2.format(tg_username, self.admin_user_role))
 
-    def get_id_user(self, tg_username : str) -> int:
+    def get_id_user(self, tg_username: str) -> int:
         """Вернем id_user из таблицы user по нику из tg. 
-        
+
         -1 если пользователя нет в базе.
         """
         assert tg_username != '', Text.User_t1_err
 
         Logger().log(Text.User_t4.format(tg_username))
         res = Database().get_from_query(Text.get_id_user_t0.format(tg_username))
-        
+
         return -1 if len(res) == 0 else res[0][0]
 
-    def is_admin(self, tg_username : str) -> bool:
+    def is_admin(self, tg_username: str) -> bool:
         """Проверяет, является ли человек админом в системе по нику в тг.
         """
         assert tg_username != '', Text.User_t1_err
-        
+
         Logger().log(Text.User_t5.format(tg_username))
         res = Database().get_from_query(
             Text.is_admin_t0.format(tg_username, self.admin_user_role))
-        
+
         return False if len(res) == 0 else True
 
-    def get_bot_username(self, tg_username : str) -> str:
+    def get_bot_username(self, tg_username: str) -> str:
         assert tg_username != '', Text.get_bot_username_err
 
         Logger().log(Text.User_t6.format(tg_username))
         res = Database().get_from_query(
             Text.get_bot_username_t0.format(tg_username))
 
-        bot_username : str = res[0][0]
+        bot_username: str = res[0][0]
         assert bot_username != '', Text.get_bot_username_err
         return bot_username
--- code/notify.py
+++ code/notify.py
@@ -7,12 +7,13 @@ from datetime import datetime, time, timedelta
 from database import Database, User
 from const import *
 
-async def NOTIFY_MENU(update: Update, 
+
+async def NOTIFY_MENU(update: Update,
                       context: ContextTypes.DEFAULT_TYPE) -> int:
-    
-    text : str = update.message.text
-    tg_username : str = update.effective_user.username
-    
+
+    text: str = update.message.text
+    tg_username: str = update.effective_user.username
+
     if text == Text.NOTIFY_MENU_KEYBOARD[0]:
         notify_list = Database().get_from_query(
             Text.NOTIFY_MENU_get_notify.format(tg_username))
@@ -22,28 +23,28 @@ async def NOTIFY_MENU(update: Update,
                 Text.NOTIFY_MENU_empty_list,
                 reply_markup=Keyboard.NOTIFY_MENU)
         else:
-            reply : str = ''
+            reply: str = ''
             for i, item in enumerate(notify_list):
                 reply += Text.NOTIFY_MENU_item_in_list.format(
                     i, item[0], item[1], item[2])
             await update.message.reply_text(
-                text=reply, 
+                text=reply,
                 reply_markup=Keyboard.NOTIFY_MENU)
-        
+
         return State.NOTIFY_MENU
-    
+
     elif text == Text.NOTIFY_MENU_KEYBOARD[1]:
         await update.message.reply_text(
             Text.NOTIFY_MENU_about_question,
             reply_markup=Keyboard.NOTIFY_MENU_ADD_CANCEL_KEYBOARD)
-        
+
         return State.NOTIFY_MENU_ADD_DESCRIPTION
-    
+
     elif text == Text.NOTIFY_MENU_KEYBOARD[2]:
-        
+
         notify_list = Database().get_from_query(
             Text.NOTIFY_MENU_get_notify.format(tg_username))
-        
+
         if len(notify_list) == 0:
             await update.message.reply_text(
                 Text.NOTIFY_MENU_empty_list,
@@ -51,87 +52,88 @@ async def NOTIFY_MENU(update: Update,
         else:
             context.user_data["notify_list_pivot"] = 0
             context.user_data["notify_list"] = notify_list
-            
-        
+
         # await update.message.reply_text(
         #     '_Сейчас удалим напоминания', )
         # return State.NOTIFY_MENU
-    
+
     elif text == Text.NOTIFY_MENU_KEYBOARD[3]:
         await update.message.reply_text(
             '_Давай изменим напоминание', )
         return State.NOTIFY_MENU_CHANGE
-    
+
     elif text == Text.NOTIFY_MENU_KEYBOARD[4]:
         await update.message.reply_text('_Что там по настройкам?', )
         return State.NOTIFY_MENU
-    
+
     elif text == Text.NOTIFY_MENU_KEYBOARD[5]:
         match User().is_admin(tg_username):
             case True:
                 await update.message.reply_text(
-                    Text.RETURN_TO_MENU_ADMIN, 
+                    Text.RETURN_TO_MENU_ADMIN,
                     reply_markup=Keyboard.MAIN_MENU_ADMIN)
-                
+
                 return State.MAIN_MENU_ADMIN
             case _:
                 await update.message.reply_text(
                     Text.RETURN_TO_MENU,
                     reply_markup=Keyboard.MAIN_MENU)
-                
+
                 return State.MAIN_MENU
-            
+
     else:
         return State.NOTIFY_MENU
 
-async def NOTIFY_MENU_ADD_DESCRIPTION(update: Update, 
-                                      context: ContextTypes.DEFAULT_TYPE)-> int:
-    
-    text : str = update.message.text
+
+async def NOTIFY_MENU_ADD_DESCRIPTION(update: Update,
+                                      context: ContextTypes.DEFAULT_TYPE):
+
+    text: str = update.message.text
 
     if text == "Отменить":
         await update.message.reply_text(
             "Возвращаемся в напоминаниям",
             reply_markup=Keyboard.NOTIFY_MENU)
-        
+
         return State.NOTIFY_MENU
-    
+
     else:
         context.user_data['NOTIFY_MENU_ADD_DESCRIPTION'] = text
         await update.message.reply_text(
-            "Какого числа? (dd.mm.yy)", 
+            "Какого числа? (dd.mm.yy)",
             reply_markup=Keyboard.get_NOTIFY_MENU_ADD_Date_pick_keyboard())
-        
+
         return State.NOTIFY_MENU_ADD_DATEPICK
 
-async def NOTIFY_MENU_ADD_DATEPICK(update: Update, 
+
+async def NOTIFY_MENU_ADD_DATEPICK(update: Update,
                                    context: ContextTypes.DEFAULT_TYPE) -> int:
-    text : str = update.message.text
+    text: str = update.message.text
 
     if text == "Отменить":
         await update.message.reply_text(
-            "Возвращаемся в напоминаниям", 
+            "Возвращаемся в напоминаниям",
             reply_markup=Keyboard.NOTIFY_MENU)
-        
+
         return State.NOTIFY_MENU
 
     if len(re.findall(r'[0-9]{1,2}[.:, ][0-9]{1,2}[.:, ][0-9]{2}', text)) == 0:
         await update.message.reply_text(
             Text.NOTIFY_MENU_ADD_DATEPICK_t0,
             reply_markup=Keyboard.get_NOTIFY_MENU_ADD_Date_pick_keyboard())
-        
+
         return State.NOTIFY_MENU_ADD_DATEPICK
 
-    dig : list[int] = list(map(int, re.findall(r'\d+', text)))
+    dig: list[int] = list(map(int, re.findall(r'\d+', text)))
     try:
         date = datetime(year=2000 + dig[2], month=dig[1], day=dig[0])
 
         if date.date() < datetime.now().date():
             await update.message.reply_text(
                 "Что то не так с вашими данными, \
-                    вы ввели дату раньше сегодня, еще раз", 
+                    вы ввели дату раньше сегодня, еще раз",
                 reply_markup=Keyboard.get_NOTIFY_MENU_ADD_Date_pick_keyboard())
-            
+
             return State.NOTIFY_MENU_ADD_DATEPICK
 
     except ValueError:
@@ -139,20 +141,21 @@ async def NOTIFY_MENU_ADD_DATEPICK(update: Update,
             Text.NOTIFY_MENU_ADD_DATEPICK_t0,
             reply_markup=Keyboard.get_NOTIFY_MENU_ADD_Date_pick_keyboard())
         return State.NOTIFY_MENU_ADD_DATEPICK
-    
+
     context.user_data['NOTIFY_MENU_ADD_DATEPICK'] = date
 
     await update.message.reply_text(
-        "Введите время (hh.mm)", 
+        "Введите время (hh.mm)",
         reply_markup=Keyboard.get_NOTIFY_MENU_ADD_Time_pick_keyboard())
-    
+
     return State.NOTIFY_MENU_ADD_TIMEPICK
 
-async def NOTIFY_MENU_ADD_TIMEPICK(update: Update, 
+
+async def NOTIFY_MENU_ADD_TIMEPICK(update: Update,
                                    context: ContextTypes.DEFAULT_TYPE):
-    
-    text : str = update.message.text
-    tg_username : str = update.effective_user.username
+
+    text: str = update.message.text
+    tg_username: str = update.effective_user.username
 
     if text == "Отменить":
         await update.message.reply_text(
@@ -161,40 +164,40 @@ async def NOTIFY_MENU_ADD_TIMEPICK(update: Update,
 
     if len(re.findall(r'[0-9]{1,2}[.:, ][0-9]{1,2}', text)) == 0:
         await update.message.reply_text(
-            Text.NOTIFY_MENU_ADD_TIMEPICK_t0, 
+            Text.NOTIFY_MENU_ADD_TIMEPICK_t0,
             reply_markup=Keyboard.get_NOTIFY_MENU_ADD_Time_pick_keyboard())
-        
+
         return State.NOTIFY_MENU_ADD_TIMEPICK
-    
-    dig : list[int] = list(map(int, re.findall(r'\d+', text)))
+
+    dig: list[int] = list(map(int, re.findall(r'\d+', text)))
 
     if not (0 <= dig[0] < 24 and 0 <= dig[1] < 60):
         await update.message.reply_text(
-            Text.NOTIFY_MENU_ADD_TIMEPICK_t0, 
+            Text.NOTIFY_MENU_ADD_TIMEPICK_t0,
             reply_markup=Keyboard.get_NOTIFY_MENU_ADD_Time_pick_keyboard())
-        
+
         return State.NOTIFY_MENU_ADD_TIMEPICK
 
-    description : str = context.user_data['NOTIFY_MENU_ADD_DESCRIPTION']
-    d : datetime = context.user_data['NOTIFY_MENU_ADD_DATEPICK']
-    t : time = time(hour=dig[0], minute=dig[1])
+    description: str = context.user_data['NOTIFY_MENU_ADD_DESCRIPTION']
+    d: datetime = context.user_data['NOTIFY_MENU_ADD_DATEPICK']
+    t: time = time(hour=dig[0], minute=dig[1])
 
     # TODO пока что мы работаем только в UTC+3, как же это решить то...?
     if d.date() == datetime.now().date() \
-        and t < (datetime.now() + timedelta(hours=3)).time():
-        
+            and t < (datetime.now() + timedelta(hours=3)).time():
+
         await update.message.reply_text(
-            Text.NOTIFY_MENU_time_err, 
+            Text.NOTIFY_MENU_time_err,
             reply_markup=Keyboard.get_NOTIFY_MENU_ADD_Time_pick_keyboard())
-        
+
         return State.NOTIFY_MENU_ADD_TIMEPICK
 
     date_str = f"{d.year}" + \
-               f"-{d.month if len(str(d.month)) == 2 else '0' + str(d.month)}"+\
-               f"-{d.day if len(str(d.day)) == 2 else '0' + str(d.day)}"
-               
+        f"-{d.month if len(str(d.month)) == 2 else '0' + str(d.month)}" +\
+        f"-{d.day if len(str(d.day)) == 2 else '0' + str(d.day)}"
+
     time_str = f"{t.hour if len(str(t.hour)) == 2 else '0' + str(t.hour)}:" + \
-               f"{t.minute if len(str(t.minute)) == 2 else '0' + str(t.minute)}"
+        f"{t.minute if len(str(t.minute)) == 2 else '0' + str(t.minute)}"
 
     Database().run_query(
         Text.NOTIFY_MENU_insert_notify.format(
@@ -202,11 +205,10 @@ async def NOTIFY_MENU_ADD_TIMEPICK(update: Update,
         ))
 
     await update.message.reply_text(
-        Text.NOTIFY_MENU_exit_add_succ, 
+        Text.NOTIFY_MENU_exit_add_succ,
         reply_markup=Keyboard.NOTIFY_MENU)
-    
-    return State.NOTIFY_MENU
 
+    return State.NOTIFY_MENU
 
 
 # async def NOTIFY_LIST():
@@ -218,9 +220,10 @@ async def NOTIFY_MENU_ADD_TIMEPICK(update: Update,
 async def NOTIFY_DELETE():
     pass
 
+
 async def NOTIFY_CHANGE():
     pass
 
+
 async def NOTIFY_SETTINGS():
     pass
-
